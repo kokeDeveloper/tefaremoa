cada1cf1e296d569bdece0f09058d0600b7e5580
Jorge Toledo <contacto@jorgetoledo.cl>
2024-07-03 19:13:34 -0400

    upgrade home

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,93 +8,76 @@ const Footer = () => {
                 particleCount={200}
                 baseHue={1}
                 rangeHue={3}>
-                <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+                <div className="container px-5 py-16 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
                     <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
                         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                             <Image
                                 src="/tefaremoa.svg"
-                                width={120}
-                                height={120}
+                                width={90}
+                                height={90}
                                 alt="TeFareLogo"
                             />
                         </a>
-                        <p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+                        <p className="mt-2 text-sm text-white">Explorando la riqueza cultural del mundo polinesio a través de la creatividad y la investigación</p>
                     </div>
-                    <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                    <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center hidden lg:contents">
                         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 uppercase">Secciones</h2>
                             <nav className="list-none mb-10">
                                 <ul>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Nosotros</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Clases</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                    </li>
-
-                                </ul>
-
-                            </nav>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
-                            <nav className="list-none mb-10">
-                                <ul>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Workshop&apos;s</a>
                                     </li>
                                 </ul>
                             </nav>
                         </div>
                         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 uppercase">Sitios de Interés</h2>
                             <nav className="list-none mb-10">
                                 <ul>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Kaipeka Tattoo</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Kari Kari</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                    </li>
-                                    <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Marketing Digital</a>
                                     </li>
                                 </ul>
                             </nav>
                         </div>
                         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 uppercase">Contacto</h2>
                             <nav className="list-none mb-10">
                                 <ul>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">WhatsApp</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">contacto@tefaremoa.com</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                            <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3 uppercase">Contacto</h2>
+                            <nav className="list-none mb-10">
+                                <ul>
+                                    <li>
+                                        <a className="text-gray-600 hover:text-gray-800">Kaipeka Tattoo</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Kari Kari</a>
                                     </li>
                                     <li>
-                                        <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                        <a className="text-gray-600 hover:text-gray-800">Marketing Digital</a>
                                     </li>
                                 </ul>
                             </nav>
@@ -103,8 +86,8 @@ const Footer = () => {
                 </div>
                 <div className="bg-black">
                     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
-                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2024 Te Fare Mo&apos;a —
+                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@koke_toledo</a>
                         </p>
                         <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
                             <a className="text-gray-500">
--- src/app/components/bannerClases.tsx
+++ src/app/components/bannerClases.tsx
@@ -2,36 +2,26 @@
 import Image from "next/image";
 import { HoverBorderGradient } from "./hover-border-gradient";
 
-
-
 export function BannerClases() {
-
     return (
-        <>
-                <section className="text-gray-600 body-font bg-black">
-                    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-                        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 text-white">Clases de Danza
-                            </h1>
-                            <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                            <div className="flex justify-center">
-                            <HoverBorderGradient containerClassName="rounded-full" as="button" className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2">
-                                <span className="uppercase">detalles</span>
-                            </HoverBorderGradient>
-                            </div>
-                        </div>
-                        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                            <Image
-                                src="/tefaremoa.svg"
-                                width={300}
-                                height={300}
-                                alt="TeFareLogo" 
-                            />
-                        </div>
+        <section className="text-gray-600 body-font bg-black relative bg-cover bg-center" style={{ backgroundImage: "url('/banner1.jpg')" }}>
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center relative z-10">
+                <div className="pr-7 lg:flex-grow md:w-1/2 lg:pr-96 md:pr-16 flex flex-col md:items-start md:text-left items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white main-title">Clases de Danza</h1>
+                    <p className="mb-8 leading-relaxed text-white">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <div className="flex justify-center">
+                        <HoverBorderGradient containerClassName="rounded-full" as="button" className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2">
+                            <span className="uppercase">conóce más</span>
+                        </HoverBorderGradient>
                     </div>
-                </section>
-        </>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
 
+                </div>
+            </div>
+            {/* Overlay para oscurecer la imagen de fondo */}
+            <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+        </section>
     );
 };
 
