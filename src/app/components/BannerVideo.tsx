fb663bf4490d77c3aa5faeaaddc4546c3595ac8b
Jorge Toledo <contacto@jorgetoledo.cl>
2025-01-03 09:45:23 -0300

    bannerVideo

---
+++ src/app/components/BannerVideo.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const BannerVideo = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover">
+        <source src="/banner1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute bottom-0 left-0 w-full h-1/6 bg-gradient-to-t from-black to-transparent"></div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-white text-center">
+        <Image src="/tefaremoa.svg" width={200} height={200} alt="TeFareLogo" />
+        <p className="mt-4 text-2xl">Academia de Danzas Polinesias<br/>From Chile</p>
+      </div>
+    </div>
+  )
+}
+
+export default BannerVideo
