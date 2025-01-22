3a52f04b796c224bda83cfa67bac9bb161eca4a1
Jorge Toledo <contacto@jorgetoledo.cl>
2025-01-22 17:41:53 -0300

    Fonts

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-reactStrictMode: true,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-        {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-            port: "",
-            pathname: "/images/**",
-        },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/images/**",
+      },
     ],
+  },
+  // Add the following configuration to serve static files
+  staticPageGenerationTimeout: 60,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ttf|otf|eot|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
+          publicPath: '/fonts/',
+        },
+      },
+    });
+    return config;
   },
 };
 
